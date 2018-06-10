d5f5bced1e4540426e799a70cf97ec1046303438
wyz <wang_yuzhong@126.com>
2018-06-10 17:39:26 +0800

    a small contribution to main

--- main.cpp
+++ main.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 #include "database.h"
 #include "server.h"
+#include "client.h"
 
+namespace LMS{
 
+void __main__(){
+	Database db=new Database("TestLMS");
+	Server server=new Server(db);
+	Client client=new Client(server);
+
+	client.main();
+
+	delete client;
+	delete server;
+	delete db;
+}
+
+};
+
+int main(){
+	LMS::__main__();
+	return 0;
+}
